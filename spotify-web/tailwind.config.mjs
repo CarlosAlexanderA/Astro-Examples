5acf17817ea04dc01d3afd34571575d841cb8245
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2024-01-24 21:46:18 -0500

    aside de spotify

--- spotify-web/tailwind.config.mjs
+++ spotify-web/tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"deep-black": "#121212",
+				"neutral-gray": "#b3b3b3",
+				"dark-slate": "#1a1a1a",
+				"shadow-gray": "#242424"
+			}
+		},
 	},
 	plugins: [],
 }
